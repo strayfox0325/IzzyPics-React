6a5f6a91a4103c6120147ce686c3ea4f0599cbe5
Isidora Lazic <isidora.lazic@yahoo.com>
2022-03-16 21:42:32 +0100

    Feed View and Pin Component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,42 @@
 /* Imports */
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {client} from '../client'
+import MasonryLayout from './MasonryLayout' // Grid layout with different div dimensions
+import Spinner from './Spinner'
+import { feedQuery, searchQuery } from '../utils/data'
 
 const Feed = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null)
+  const {categoryId} = useParams();
+  useEffect(() => {
+    setLoading(true);
+
+    if(categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data)
+        setLoading(false)
+      })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data)
+        setLoading(false)
+      })
+    }
+  }, [categoryId])
+
+  if(loading) return <Spinner message="Adding New Content to Your Feed" />
 
 /* Render */
 
   return (
-    <div>Feed</div>
+    <div>
+      {pins && <MasonryLayout pins={pins}/>}
+    </div>
   )
 }
 
