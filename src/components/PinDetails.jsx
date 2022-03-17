0c5a320532e499b355ae3617cdcfc396ce9e19fc
Isidora Lazic <isidora.lazic@yahoo.com>
2022-03-17 11:14:33 +0100

    Fixed Pin onClick + minor style changes

--- src/components/PinDetails.jsx
+++ src/components/PinDetails.jsx
@@ -1,8 +1,25 @@
   /* Imports */
 
-  import React from 'react'
-  
-  const PinDetails = () => {
+import React, {useState, useEffect} from 'react'
+import { MdDownloadForOffline } from 'react-icons/md'
+import { Link, useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
+import { client, urlFor } from '../client'
+import MasonryLayout from './MasonryLayout'
+import { pinDetailsMorePinQuery, pinDetailsQuery} from '../utils/data'
+import Spinner from './Spinner'
+
+  const PinDetails = ({user}) => {
+
+    /* Hooks */
+
+    const [pins, setPins] = useState(null)
+    const [pinDetails, setPinDetails] = useState(null)
+    const [comment, setComment] = useState('')
+    const [addingComment, setAddingComment] = useState(false)
+    const { pinId } = useParams()
+
+    if(!pinDetails) return <Spinner message="Loading Pin"/>
 
     /* Render */
 
