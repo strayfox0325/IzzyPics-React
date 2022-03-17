3a17f1aae664125901e6d0f123233fed8dab2aa7
Isidora Lazic <isidora.lazic@yahoo.com>
2022-03-17 14:05:24 +0100

    Categories and Search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,50 @@
   /* Imports */
 
-  import React from 'react'
-  
-  const Search = () => {
+import React, {useState, useEffect } from 'react'
+import MasonryLayout from './MasonryLayout'
+import { client } from '../client'
+import { feedQuery, FeedQuery, searchQuery, SearchQuery} from '../utils/data'
+import Spinner from './Spinner'
 
+  const Search = ({searchTerm}) => {
+
+    /* Hooks */
+
+    const [pins, setPins] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+      if(searchTerm) {
+        setLoading(true)
+        const query = searchQuery(searchTerm.toLowerCase())
+        client
+        .fetch(query)
+        .then((data) => {
+          setPins(data)
+          setLoading(false)
+        })
+      } else {
+        client
+        .fetch(feedQuery)
+        .then((data) => {
+          setPins(data)
+          setLoading(false)
+        })
+      }
+    }, [searchTerm])
+    
     /* Render */
 
     return (
-      <div>Search</div>
+      <div>
+        {loading && <Spinner message="Looking for Pins..."/>}
+        {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+        {pins?.length === 0 && searchTerm !== '' && !loading && (
+          <div className="mt-10 text-center text-xl">
+            No Pins Found
+          </div>
+        )}
+      </div>
     )
   }
   
